4e2ebaf062da4ebb730ac71537af1b6685257c4c
Lohins <coneonsitong@gmail.com>
2016-08-08 17:17:39 -0300

    Post

---
+++ Bridge_Header.h
@@ -0,0 +1,17 @@
+//
+//  Bridge_Header.h
+//  Stripe-Test
+//
+//  Created by Sitong on 2016-08-08.
+//  Copyright © 2016 Sitong. All rights reserved.
+//
+
+#ifndef Bridge_Header_h
+#define Bridge_Header_h
+
+
+#import "AFNetworking.h"
+#import "Stripe/Stripe.h"
+
+#endif /* Bridge_Header_h */
+
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stripe-Test
+//
+//  Created by Sitong on 2016-08-08.
+//  Copyright © 2016 Sitong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
